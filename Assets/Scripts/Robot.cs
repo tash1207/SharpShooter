a6e1963f1b4be4e0427a8c4e79f4f996e0d7921f
Tasha Hankewych <thankewych@gmail.com>
2026-07-18 22:26:59 -0500

    Create a nav mesh surface and nav mesh agent

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,24 @@
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Robot : MonoBehaviour
+{
+    FirstPersonController player;
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    void Start()
+    {
+        player = FindFirstObjectByType<FirstPersonController>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
