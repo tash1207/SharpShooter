d3f21eae204fdcf00ed814df6cb472991e2ad2cb
Tasha Hankewych <thankewych@gmail.com>
2026-09-09 16:09:15 -0500

    Implement a cooldown rate for firing weapon

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,45 +1,22 @@
-using StarterAssets;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] WeaponSO weaponSO;
-    [SerializeField] GameObject hitVFXPrefab;
-    [SerializeField] Animator animator;
     [SerializeField] ParticleSystem muzzleFlash;
 
-    StarterAssetsInputs starterAssetsInputs;
-
-    const string SHOOT_STRING = "Shoot";
-
-    void Awake()
+    public bool Shoot(WeaponSO weaponSO)
     {
-        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
-    }
-
-    void Update()
-    {
-        HandleShoot();
-    }
-
-    private bool HandleShoot()
-    {
-        if (!starterAssetsInputs.shoot) return false;
-
         muzzleFlash.Play();
-        animator.Play(SHOOT_STRING, 0, 0f);
-
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
-            Instantiate(hitVFXPrefab, hit.point, Quaternion.identity);
+            Instantiate(weaponSO.HitVFXPrefab, hit.point, Quaternion.identity);
             if (hit.collider.gameObject.TryGetComponent<EnemyHealth>(out EnemyHealth enemyHealth))
             {
                 enemyHealth.TakeDamage(weaponSO.Damage);
             }
         }
-        starterAssetsInputs.ShootInput(false);
         return true;
     }
 }
